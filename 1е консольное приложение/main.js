f33da7f61e68e40af4e6c001499346261aa602a8
Anton <neftnam@gmail.com>
2020-09-16 12:43:39 +0300

    'coundown'

--- 1е консольное приложение/main.js
+++ 1е консольное приложение/main.js
@@ -1,80 +1,80 @@
 "use strict";
 
-
-let numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-let a;
-let b;
-
-function start() {
-    numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    while (numberofFilms === '' || numberofFilms === null || isNaN(numberofFilms)) {
-        numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
-start();
-
 const personalMovieDB = {
-    count: numberofFilms,
+    count: 0,
     movies: {
     },
     actors: {},
     genres: [],
     privat: false,
-};
+    start: function () {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        while (personalMovieDB.count === '' || personalMovieDB.count === null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Один из последних просмотренных фильмов?", ""),
+                b = prompt("На сколько вы его оцените?", "");
 
+            if (a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 50) {
 
+            }
+            else {
+                alert("Please refresh the page and fill all the fields");
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count >= 1 && personalMovieDB.count < 10) {
+            alert("Not enough movies have been watched");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert("You are a classical viewer");
+        } else if (30 <= personalMovieDB.count) {
+            alert("Wow");
+        } else if (personalMovieDB.count <= 0) {
+            alert("Error");
+        }
 
-//* Задание - 2
-//Автоматизируем вопросы
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        a = prompt("Один из последних просмотренных фильмов?", "");
-        b = prompt("На сколько вы его оцените?", "");
-
-        if (a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 50) {
-
+        personalMovieDB.movies[a] = b;
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function (shown) {
+        if (!shown) {
+            personalMovieDB.privat = true;
         }
         else {
-            alert("Please refresh the page and fill all the fields");
-            i--;
+            personalMovieDB.privat = false;
+        }
+    },
+    writeYourGenres: function (noSkipGenres) {
+        let i;
+        for (i = 1; i <= 3; i++) {
+            const genres = prompt(`Ваш любимый жанр под номером ${i}`);
+            noSkipGenres[i - 1] = genres;
+            if (genres === "" || genres === null) {
+                i--;    
+            }
+            else {
+                noSkipGenres.forEach(function (genre, number) {
+                    console.log(`Любимый жанр № ${number + 1} - это ${genre}`);
+                });
+            }
         }
     }
-}
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count >= 1 && personalMovieDB.count < 10) {
-        alert("Not enough movies have been watched");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert("You are a classical viewer");
-    } else if (30 <= personalMovieDB.count) {
-        alert("Wow");
-    } else if (personalMovieDB.count <= 0) {
-        alert("Error");
-    }
-
-    personalMovieDB.movies[a] = b;
-}
-detectPersonalLevel();
-// function showMyDB() { //!Это простой код,который написал я, он рабочий. ниже будет приведен код записанный через аргумент
-//     if (personalMovieDB.privat == false) {
-//         console.log(personalMovieDB);
-//     }
-// }
-// showMyDB();
-function showMyDB(hidden){ //[1]// !Важно! тут мы имеем дело с булиновыми значениями и в [2] мы создадим условие,которое будет проверять является ли это значение false
-    if (!hidden){//[2]"if(!hidden)" Если hidden = false,то....
-        console.log(personalMovieDB);//[3]
-    }
-} 
-showMyDB(personalMovieDB.privat);//[4] Аргумент,который помещается в [1]
-
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимы жанр под номером ${i}`, "");; //если не записать i - 1, то в массиве будет 4 элемента,первый из которых empty.
-    }
-
-}
-writeYourGenres();
+};
+// personalMovieDB.start();
+// personalMovieDB.rememberMyFilms();
+// personalMovieDB.detectPersonalLevel();
+// personalMovieDB.showMyDB(personalMovieDB.privat);
+// personalMovieDB.toggleVisibleMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres(personalMovieDB.genres);
 console.log(personalMovieDB);
+
+
